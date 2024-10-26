8af37b01d44e54746f80a38ab03abf4167b6791a
Kandugula-poojitha <kandugulapoojitha1@gmail.com>
2024-10-26 07:48:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -80,7 +80,7 @@ class WeatherApp(QWidget):
         self.get_weather_button.clicked.connect(self.get_weather)
     def get_weather(self):
         
-        api_key="d6bb3c0f8b5a1f08287204e8b4ba57cb"
+        api_key="**********"
         city=self.city_input.text()
         url=f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
 
